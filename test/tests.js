f97f0438f53b7da4ec955220c6f040553cb7ba3d
fora-heims <foraheims@gmail.com>
2021-08-26 15:34:30 -0700

    TDD for all features added back to tests file and overall style updated

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,55 @@
 // IMPORT MODULES under test here:
-import { divideFunction } from '../calculator.js';
+import { addFunction, subtractFunction, multiplyFunction, divideFunction } from '../calculator.js';
 
 const test = QUnit.test;
+
+// name your test by what it is testing
+test('add function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 25;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = addFunction(20, 5); // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+// name your test by what it is testing
+test('subtract function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 15;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtractFunction(20, 5); // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+// name your test by what it is testing
+test('multiply function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 100;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiplyFunction(20, 5); // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
 
 // name your test by what it is testing
 test('divide function', (expect) => {
