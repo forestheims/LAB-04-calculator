4c314243f6959fbd1a012c440575c0718dc649db
fora-heims <foraheims@gmail.com>
2021-08-26 13:30:10 -0700

    Added add UI and button listener

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // import functions
 
 // reference needed DOM elements
-
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
 
 // set event listeners 
     // get user input(s)
+addButton.addEventListener('click', () => {
+    console.log('Add Button clicked');
+});
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
