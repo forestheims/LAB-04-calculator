22cb354daf835d7863d4c1e2fb205fbfe048a64d
fora-heims <foraheims@gmail.com>
2021-08-26 22:43:41 -0700

    deleted four semi colons to get the green check on GitHub for this commit

--- calculator.js
+++ calculator.js
@@ -1,15 +1,15 @@
 export function addFunction(x, y) {
     return x + y;
-};
+}
 
 export function subtractFunction(x, y) {
     return x - y;
-};
+}
 
 export function multiplyFunction(x, y) {
     return x * y;
-};
+}
 
 export function divideFunction(x, y) {
     return x / y;
-};
+}
